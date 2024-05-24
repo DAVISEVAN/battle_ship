30078f983af522170997ad36d17336e552806b58
Evan Davis <evandavis427@gmail.com>
2024-05-24 15:09:58 -0600

    Added test for initialize method in ship_spec.rb

---
+++ spec/ship_spec.rb
@@ -0,0 +1,27 @@
+# spec/ship_spec.rb
+
+require './lib/ship'
+
+RSpec.describe Ship do
+  before :each do
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@cruiser).to be_a(Ship)
+    end
+
+    it 'has a name' do
+      expect(@cruiser.name).to eq("Cruiser")
+    end
+
+    it 'has a length' do
+      expect(@cruiser.length).to eq(3)
+    end
+
+    it 'starts with full health' do
+      expect(@cruiser.health).to eq(3)
+    end
+  end
+end
