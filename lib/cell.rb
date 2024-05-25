2ca27acd0dfff72fa1be6ae6eaafff90e1fc3e87
Cory Bretsch <corybretsch@gmail.com>
2024-05-24 20:59:16 -0500

    feat: class Cell #render

--- lib/cell.rb
+++ lib/cell.rb
@@ -20,12 +20,24 @@ class Cell
   end
   
   def fire_upon
-    @ship.hit
     @counter +=1 
+    if @ship != nil
+      @ship.hit
+    end
   end
 
-
-
-
+  def render(show_ship = false)
+    if show_ship == true
+      "S"
+    elsif fired_upon? == false
+      "."
+    elsif fired_upon? == true && @ship == nil
+      "M"
+    elsif @ship.sunk? == true
+      "X"
+    else fired_upon? == true && @ship != nil
+      "H"
+    end
+  end
 
 end
