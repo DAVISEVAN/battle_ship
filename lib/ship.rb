506af3ad19f2ee9d262b23fb88f9efb333b1c1e8
Cory Bretsch <corybretsch@gmail.com>
2024-05-24 19:45:24 -0500

    feat: class Ship #hit and #sunk?

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,18 +1,21 @@
 class Ship
-   attr_reader :name, :length
+  attr_reader :name, :length
 
-def initialize(name, length)
-  @name = name
-  @length = length
-end
+  def initialize(name, length)
+    @name = name
+    @length = length
+  end
 
-def health
-  @length
-end
+  def health
+    @length
+  end
 
+  def hit
+    @length -= 1
+  end
 
-
-
-
+  def sunk?
+    @length == 0
+  end
 
 end
