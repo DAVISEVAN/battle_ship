35cfe461c508a8520627512ebd1bd18238a4ca63
Evan Davis <evandavis427@gmail.com>
2024-05-27 07:24:31 -0600

    Added game class file

---
+++ lib/game.rb
@@ -0,0 +1,29 @@
+class Game
+    def initialize
+      @player_board = Board.new
+      @computer_board = Board.new
+      @player_ships = []
+      @computer_ships = []
+    end
+    
+    def start
+      
+    end
+    
+    def main_menu
+      
+    end
+  
+    def setup
+      
+    end
+  
+    def turn
+      
+    end
+  
+    def end_game
+     
+    end
+  end
+  
