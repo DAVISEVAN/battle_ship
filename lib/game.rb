461ac5db4cf2b29134e6657a589bb5a9c3d2d7bd
Cory Bretsch <corybretsch@gmail.com>
2024-05-27 20:00:48 -0500

    test: game_spec

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,9 @@
 class Game
+  attr_reader :player_board, 
+              :computer_board, 
+              :player_ships, 
+              :computer_ships
+              
   def initialize
     @player_board = Board.new
     @computer_board = Board.new
